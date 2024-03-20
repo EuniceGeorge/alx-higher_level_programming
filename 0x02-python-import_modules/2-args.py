ff553f2280555ff8381e05971308ab7a40569c35
Eunice George <georgeunice408@gmail.com>
2024-03-20 09:26:33 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    val = ""
+    argc = len(argv) - 1
+    if argc > 1:
+        delim = ":s"
+    elif argc == 0:
+        delim = "."
+    else:
+        delim = ":"
+    for i, ag in enumerate(argv[1:]):
+        val += '{:d}: {}\n'.format(i+1, ag)
+    print('{:d} argument{}\n{}'.format(argc, delim, val), end="")
