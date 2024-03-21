a0e658d2483d67b77fa7a847ef4e34bcd0a15738
Eunice George <georgeunice408@gmail.com>
2024-03-21 09:20:14 -0700

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    
+    if len(my_list) == 0:
+        return None
+    maxlen = my_list[0]
+    for num in my_list[1:]:  # Or range(1, len(my_list)):
+        if maxlen < num:
+            maxlen = num
+    return maxlen
