c44770013c1648237e39a2efe6385b42da27d243
Eunice George <georgeunice408@gmail.com>
2024-04-03 19:29:50 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-
 class Square:
     pass
+    """
+    A class to define empty squares
+    
+    pass:
+
+        a place holder indicating an empty block
+        """
+        
