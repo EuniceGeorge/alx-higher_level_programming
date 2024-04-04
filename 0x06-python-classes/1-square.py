870930bb62d1a200485985228d3945b84e1c97b2
Eunice George <georgeunice408@gmail.com>
2024-04-03 19:53:01 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+class Square:
+    """
+    a class that perform square.
+
+    Attributes:
+        size(int): The number to passed.
+        """
+    def __init__(self, size):
+
+        """
+        initialization of square number
+        Args:
+            size(int): the number
+            """
+        self.size = size
