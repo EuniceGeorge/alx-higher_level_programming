a79727ebbe68a930b583e48c227ebf72b1e5db1a
Eunice George <georgeunice408@gmail.com>
2024-04-03 21:45:02 -0700

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class that represents a square and calculates its area.
+
+    Attributes:
+    - size (int): The size of the square's side.
+
+    Methods:
+    - __init__(size=0): Initializes the Square instance with a specified size.
+    - area(): Calculates and returns the area of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square object with a specified size.
+
+        Parametrs:
+        - size (int): The size of the square's side. Default is 0.
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+        - int: The area of the square (size squared).
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Get the size of the square.
+
+         Returns:
+            int: The size of the square.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size to set for the square.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        for _ in range(self.__size):
+            for _ in range(self.__size):
+                print('#', end="")
+            print()
