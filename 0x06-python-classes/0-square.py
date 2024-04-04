ed3fb24fea915e9faef4a07231de3b285338ea76
Eunice George <georgeunice408@gmail.com>
2024-04-04 01:32:32 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+"""
+
+
 class Square:
     """
     This is an empty class named EmptyClass.
