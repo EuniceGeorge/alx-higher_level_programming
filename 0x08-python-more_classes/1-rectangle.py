9afe752bb82315ac92f800131538943de81c938e
Eunice George <georgeunice408@gmail.com>
2024-04-10 05:34:27 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,10 +24,14 @@ class Rectangle:
 
     @property
     def width(self):
+        """get access to private instance width
+        return width"""
         return(self.__width)
 
     @width.setter
     def width(self, value):
+        """Raise Typeerror if width is not integer
+        valueerror if width is less than 0"""
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
@@ -36,10 +40,14 @@ class Rectangle:
 
     @property
     def height(self):
+        """get access to private instance height
+        return height"""
         return(self.__height)
 
     @height.setter
     def height(self, value):
+        """Raise Typeerror if width is not integer
+        valueerror if width is less than 0"""
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
