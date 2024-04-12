7f783353f84543f05a26f5275bf50d678c8516f3
Eunice George <georgeunice408@gmail.com>
2024-04-12 07:21:55 -0700

    task 3

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """
 This module divides all elements of a matrix by an integer
-
-The matrix must be a list of lists of integers/floats
-Each row of the matrix must be thesame size
-
-else it raises an error
 """
 
 
@@ -13,11 +8,14 @@ def matrix_divided(matrix, div):
 
     """
     It returns the result of the divivded matrix
+    >>>matrix_divided()
     """
-
-
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
 
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
@@ -30,14 +28,16 @@ def matrix_divided(matrix, div):
         """
         scan through all the row excluding the first row
         """
-        raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError("Each row of the matrix"
+                        "must have the same size")
 
     result = []
     for row in matrix:
         new_row = []
         for i in row:
             if not isinstance(i, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix"
+                                "(list of lists) of integers/floats")
             new_row.append(round(i / div, 2))
         result.append(new_row)
     return result
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+This module prints the first_name and last_name_
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """print name: my name is {first_name} {second_name}"""
     if not isinstance(first_name, str):
+        """check if first name is a string"""
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
+        """check if last name is a string"""
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format (first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
