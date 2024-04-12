654a154e0930f6c0627d9a0590c33f3a09efd4ad
Eunice George <georgeunice408@gmail.com>
2024-04-12 09:46:11 -0700

    test 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""
+A module that prints text"""
+
+
 def text_indentation(text):
+    """
+    text_indentation function:
+    checks to see if input is valid
+    adds two new lines after any instances of `?` or `.` or `:`
+    then prints the result without any new lines at the beginning
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
     result = ""
     for words in text:
         result += words
         if words in (".", "?", ":"):
             result += "\n\n"
     print(result)
-
