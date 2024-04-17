899ddfd13cf18f8609f7ed496bf2e238024d169e
Eunice George <georgeunice408@gmail.com>
2024-04-17 05:33:38 -0700

    task 4 to 8

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" An empty class """
+
+
+class BaseGeometry:
+    """ A class with no argument"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A function based on previous"""
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
