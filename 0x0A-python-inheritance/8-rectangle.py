24b3da00989d607fa3adbbb39260628eba3451ca
Eunice George <georgeunice408@gmail.com>
2024-04-17 06:27:13 -0700

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class BaseGeometry:
-    """ A class BaseGeometry 
+    """ A class BaseGeometry
     Args:
             name (str): The name of the value being validated.
             value (int): The value to be validated.
@@ -12,7 +12,6 @@ class BaseGeometry:
             TypeError: If the value is not an integer.
             ValueError: If the value is less than or equal to 0.
         """
-
     def area(self):
         raise Exception("area() is not implemented")
 
