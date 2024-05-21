94412030ea5ed1f5a455033bedf8652049605fee
EuniceGeorge <georgeunice408.com>
2024-05-21 16:54:29 +0100

    task 4

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
--- script that lists all the tables of a database in your MySQL server
-USE mysql;
--- List all tables in the selected database
-
+-- script that lists all the tables of a database in your MySQL server;
 SHOW TABLES;
