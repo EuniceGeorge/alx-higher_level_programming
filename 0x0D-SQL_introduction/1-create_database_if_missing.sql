1ad3fdd982b462a97d4f202b5ac602029dd3b65a
EuniceGeorge <georgeunice408.com>
2024-05-21 15:28:06 +0100

    task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,6 @@
 -- script that creates the database hbtn_0c_0 in your MySQL server
-CREATE DATABASE hbtn_0c_0;
+DBNAME = "hbtn_0"
+DBEXIST = $(mysql --batch --skip-column-names -e "SHOW DATABASES LIKE '"$DBNAME"';" | grep "$DBNAME" > /dev/null; echo "$?")
+if [ $DBEXIST -eq 0 ];then
+	exit;
+fi
