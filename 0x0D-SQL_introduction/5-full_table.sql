74405146cf9ec9606624870499ce3e11b868b095
EuniceGeorge <georgeunice408.com>
2024-05-23 13:25:40 +0100

    task 7

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT *
-FROM information_schema.columns
-WHERE table_name = 'first_table';
+-- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL 
+SHOW CREATE TABLE first_table;
