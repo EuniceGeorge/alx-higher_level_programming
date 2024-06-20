36162d443f5b50cd80654f1471dd7665960590bd
EuniceGeorge <georgeunice408.com>
2024-06-20 02:10:41 +0100

    task

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,10 @@ const args = process.argv;
 const x = args[2];
 let i;
 
-if (isNaN(parseInt(x)) === true){
-	console.log('Missing number of occurrences');
+if (isNaN(parseInt(x)) === true) {
+  console.log('Missing number of occurrences');
 } else {
-	for (i = 0; i < x; i++){
-		console.log('C is fun');
-	}
+  for (i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
