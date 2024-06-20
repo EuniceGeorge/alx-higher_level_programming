ef6df333a6e6474a1a53a73b7cdcc32e702c2983
EuniceGeorge <georgeunice408.com>
2024-06-20 14:58:23 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
-      const empty = {};
-    } else {
-      this.width = w;
-      this.height = h;
-    }
+    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) { return; }
+
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
