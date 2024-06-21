72270090fca452954f605cf9a033a55016f06f9a
EuniceGeorge <georgeunice408.com>
2024-06-21 16:23:33 +0100

    task1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""script that lists all states from the database """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
