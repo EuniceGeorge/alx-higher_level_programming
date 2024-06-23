19179e96989b21c0611edbf70efd846e7c755055
EuniceGeorge <georgeunice408.com>
2024-06-23 23:31:54 +0100

    task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,21 +4,21 @@
 """script that lists all states from the database """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
 
-    user, pwrd, dbase, n = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     conn = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=user,
-            passwd=pwrd,
-            db=dbase,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
             charset="utf8")
     cur = conn.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(n))
+            "SELECT * FROM states WHERE name LIKE '{}' \
+                    ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         if row[1] == argv[4]:
