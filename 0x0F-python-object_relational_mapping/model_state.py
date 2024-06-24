54951aea0213fb8a8d065041f90b8772a74b743a
EuniceGeorge <georgeunice408.com>
2024-06-24 18:14:12 +0100

    task 0

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
+""" Start link class to table in database """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+
+Base.metadata.create_all(engine)
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer(), primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+
+
+session = Session()
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+session.close()
