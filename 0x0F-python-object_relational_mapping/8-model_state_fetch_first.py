320515ba945355d5cf80dc31cb3742cbf615831d
EuniceGeorge <georgeunice408.com>
2024-06-25 13:17:46 +0100

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Script that prints the first State object from the database
 hbtn_0e_6_usa"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    first = session.query(State).order_by(State.id).first()
+    first = session.query(State.id, State.name).first()
     if first:
         print("{}: {}".format(first.id, first.name))
     else:
