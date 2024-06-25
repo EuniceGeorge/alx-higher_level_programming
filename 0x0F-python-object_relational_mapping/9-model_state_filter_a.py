9d4c003484b3dd1e64ca39e4e27b7a2dc434fc64
EuniceGeorge <georgeunice408.com>
2024-06-25 13:33:55 +0100

    task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,4 +18,5 @@ if __name__ == "__main__":
                 if state:
                     print("{}: {}".format(state.id, state.name))
                 else:
+                    break
     session.close()
