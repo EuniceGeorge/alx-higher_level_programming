d54e85f13deb3a6117a91a906024c04b8508b69c
EuniceGeorge <georgeunice408.com>
2024-06-25 09:28:30 +0100

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ Create the 'states' table. """
     __tablename__ = 'states'
     id = Column(Integer(), primary_key=True, autoincrement=True,
                 nullable=False, unique=True)
