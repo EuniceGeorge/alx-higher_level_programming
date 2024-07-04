ee217a46da851b6ba6658bd80df05ec74951d88f
EuniceGeorge <georgeunice408.com>
2024-07-04 12:53:06 +0100

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays the size of the body of the response
-curl -s"$i" | grep  "Content-Length:" 
+curl -sI "$1" |echo " $headers" grep "Content-Length:"| cut -d":" -f2
