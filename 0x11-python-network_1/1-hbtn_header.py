24470b7d27a5aaced71b38967062ec1576fb3e2e
EuniceGeorge <georgeunice408.com>
2024-07-05 14:10:38 +0100

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,10 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """ A script that sends a request to the URL and displays the value of the X-Request-Id """
-
+import sys
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.GET
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
+
+
