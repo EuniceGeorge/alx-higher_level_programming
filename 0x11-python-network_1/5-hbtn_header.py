2581194e338c793f892983142b0cdf1ac3addcc6
EuniceGeorge <georgeunice408.com>
2024-07-06 02:06:24 +0100

    task 4

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,9 @@ in the response header"""
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
+
     response = requests.get(url)
     print(response.headers.get('X-Request-Id'))
