156ffaffe786953def89a3f319bc1393b7586d9f
EuniceGeorge <georgeunice408.com>
2024-07-06 02:18:12 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
 """script that fetches https://intranet.hbtn.io/status"""
-
 import requests
+
 
 if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
